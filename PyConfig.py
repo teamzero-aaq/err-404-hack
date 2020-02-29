07bd0366a97d9ff61711a06d2d481c8806ca8f1f
teamzero-aaq <abdulquadir828@hmail.com>
2020-02-29 12:28:21 +0530

    first commit

---
+++ PyConfig.py
@@ -0,0 +1,10 @@
+config1 = {
+    "apiKey": "AIzaSyArPSVhwHv-rxxehxy0OUCE1ILCGXp17bI",
+    "authDomain": "scholar-help-966a2.firebaseapp.com",
+    "databaseURL": "https://scholar-help-966a2.firebaseio.com",
+    "projectId": "scholar-help-966a2",
+    "storageBucket": "scholar-help-966a2.appspot.com",
+    "messagingSenderId": "988235258939",
+    "appId": "1:988235258939:web:14290d685174b9461e901e",
+    "measurementId": "G-RM8V5QZ205"
+}
